943018a22815d18e6375315bc89815b884c463e9
luowei <luowei010101@gmail.com>
2014-05-26 16:30:38 +0800

    添加了自定义Button的示例，介绍了PaintCode

---
+++ CustomButton/CustomButton/CustomButton.h
@@ -0,0 +1,13 @@
+//
+//  CustomButton.h
+//  CustomButton
+//
+//  Created by luowei on 14-5-26.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomButton : UIButton
+
+@end
---
+++ CustomButton/CustomButton/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CustomButton
+//
+//  Created by luowei on 14-5-26.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
