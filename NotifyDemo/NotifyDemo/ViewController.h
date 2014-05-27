348fc8a3704ee02eb640d60c0559d912d6e07d22
luowei <luowei010101@gmail.com>
2014-05-28 00:04:14 +0800

    添加了一个显示本地通知的示例

---
+++ NotifyDemo/NotifyDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NotifyDemo
+//
+//  Created by luowei on 14-5-27.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
